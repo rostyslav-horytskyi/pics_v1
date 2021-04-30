096a680a93b7c5160bf674d5a2fdc7ddeb975649
gorzer <gorzer.rostyslav@gmail.com>
2021-04-30 14:29:41 +0300

    Created submit event handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,12 +5,21 @@ class SearchBar extends React.Component {
     term: '',
   }
 
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state.term);
+  }
+
   render() {
     const { term } = this.state;
 
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form
+          className="ui form"
+          onSubmit={this.onFormSubmit}
+        >
           <div className="field">
             <label>Image Search</label>
             <input
